3cfd520f28f23dbc2d1f35c2200e6903cb2907b1
jason <jason2594746793@gmail.com>
2023-03-27 23:52:39 +0100

    commit from vscode

--- trader_j.py
+++ trader_j.py
@@ -1,145 +1,131 @@
-from typing import Dict, List
-from datamodel import OrderDepth, TradingState, Order
+import json
+from typing import Dict, List, Any
+from datamodel import *
 from collections import defaultdict
 
+
+class Logger: 
+    # Set this to true, if u want to create
+    # local logs
+    local: bool
+    # this is used as a buffer for logs
+    # instead of stdout
+    local_logs: dict[int, str] = {}
+
+    def __init__(self, local=False) -> None:
+        self.logs = ""
+        self.local = local
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]]) -> None:
+        output = json.dumps({
+            "state": state,
+            "orders": orders,
+            "logs": self.logs,
+        }, cls=ProsperityEncoder, separators=(",", ":"), sort_keys=True)
+        if self.local:
+            self.local_logs[state.timestamp] = output
+        print(output)
+
+        self.logs = ""
+
+
 class Trader:
+
+    logger = Logger(local=True)
+
     def __init__(self) -> None:
         self.trade_prices = defaultdict(list)
 
-    def calculate_fair_value(self, product) -> float:
-        MIN_SAMPLE_SIZE = 5
-        MAX_SAMPLE_SIZE = 100
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
 
-        # Remove earlier data points
-        self.trade_prices[product] = self.trade_prices[product][-MAX_SAMPLE_SIZE:]
-        prev_trades = self.trade_prices[product]
-        if len(prev_trades) < MIN_SAMPLE_SIZE:
-            return None
+        result = {}
+        result["PEARLS"] = self.trade_pearls(state)
+        result["BERRIES"] = self.trade_berries(state)
+        result["COCONUTS"] = self.trade_coconut_pinacoladas(state, "COCONUTS")
+        result["PINA_COLADAS"] = self.trade_coconut_pinacoladas(state, "PINA_COLADAS")
 
-        # finding the weighted average
-        total_volume, total_quantity = 0, 0
-        for trade in prev_trades:
-            total_volume += trade.price * trade.quantity
-            total_quantity += trade.quantity
+        self.logger.flush(state, result)
+        return result
 
-        return total_volume / total_quantity
+    def trade_pearls(self, state: TradingState) -> List[Order]:
+        product = "PEARLS"
+        limit = 20
+        fair_value = 10000
+        order_depth, position = state.order_depths.get(
+            product, None), state.position.get(product, 0)
+        if not order_depth:
+            return []
+        orders = []
+
+        if order_depth.sell_orders and position < limit:
+            asks = sorted(order_depth.sell_orders.keys())
+            for ask in asks:
+                if ask < fair_value:
+                    volume = order_depth.sell_orders[ask]
+                    orders.append(Order(product, ask, -volume))
+        if order_depth.buy_orders and position > -limit:
+            bids = sorted(order_depth.buy_orders.keys(), reverse=True)
+            for bid in bids:
+                if bid > fair_value:
+                    volume = order_depth.buy_orders[bid]
+                    orders.append(Order(product, bid, -volume))
+        return orders
+
+    def trade_berries(self, state: TradingState) -> List[Order]:
+        product = "BERRIES"
+        limit = 250
+        order_depth, position, timestamp = state.order_depths.get(
+            product, None), state.position.get(product, 0), state.timestamp
+        if not order_depth:
+            return []
+
+        # # backtesting
+        # buy_ends_at = 30 * 1000
+        # sell_starts_at = 50 * 1000
+
+        # production
+        buy_ends_at = 300 * 1000
+        sell_starts_at = 500 * 1000
+
+        if timestamp < buy_ends_at and order_depth.sell_orders and position < limit:
+            best_ask = min(order_depth.sell_orders.keys())
+            volume = order_depth.sell_orders[best_ask]
+            return [Order(product, best_ask, -volume)]
+        if timestamp > sell_starts_at and order_depth.buy_orders and position > -limit:
+            best_bid = max(order_depth.buy_orders.keys())
+            volume = order_depth.buy_orders[best_bid]
+            return [Order(product, best_bid, -volume)]
+        return []
     
-    def process_statistical_arbitrage_orders(self, state: TradingState, product: str, fair_value: float) -> Order:
+    def trade_coconut_pinacoladas(self, state: TradingState, product: str) -> List[Order]:
+        orders = []
+
         # Get the related product
         related_product = "COCONUTS" if product == "PINA_COLADAS" else "PINA_COLADAS"
 
-        # Calculate the fair value of the related product
-        related_fair_value = self.calculate_fair_value(related_product)
-
-        # Compare the fair values of the two products and implement statistical arbitrage
-        order_depth: OrderDepth = state.order_depths[product]
-        related_order_depth: OrderDepth = state.order_depths[related_product]
-
-        # Buy the product if its fair value is lower than the related product's fair value
-        if fair_value < related_fair_value and len(order_depth.sell_orders) > 0:
-            best_ask = min(order_depth.sell_orders.keys())
-            best_ask_volume = order_depth.sell_orders[best_ask]
-            print("BUY", str(-best_ask_volume) + "x", best_ask)            
-            return Order(product, best_ask, -best_ask_volume)
-
-        # Sell the product if its fair value is higher than the related product's fair value
-        elif fair_value > related_fair_value and len(order_depth.buy_orders) > 0:
-            best_bid = max(order_depth.buy_orders.keys())
-            best_bid_volume = order_depth.buy_orders[best_bid]
-            print("SELL", str(best_bid_volume) + "x", best_bid)
-            return Order(product, best_bid, -best_bid_volume)
+        # get the order depth and position of the products
+        c_order_depth: OrderDepth = state.order_depths["COCONUTS"]
+        pc_order_depth: OrderDepth = state.order_depths["PINA_COLADAS"]
         
+        # Get the best ask and bid prices and volumes for coconut
+        c_best_ask = min(c_order_depth.sell_orders.keys())
+        c_best_ask_volume = c_order_depth.sell_orders[c_best_ask]
+        c_best_bid = max(c_order_depth.buy_orders.keys())
+        c_best_bid_volume = c_order_depth.buy_orders[c_best_bid]
+        # Get the best ask and bid prices and volumes for pina colada
+        pc_best_ask = min(pc_order_depth.sell_orders.keys())
+        pc_best_ask_volume = pc_order_depth.sell_orders[pc_best_ask]
+        pc_best_bid = max(pc_order_depth.buy_orders.keys())
+        pc_best_bid_volume = pc_order_depth.buy_orders[pc_best_bid]
 
-    def process_diving_gear_orders(self, state: TradingState, product: str, fair_value: float, dolphin_sightings: int) -> Order:
+        if pc_best_bid / c_best_ask > 1.876:
+            orders.append(Order(product, c_best_ask, -c_best_ask_volume)) if product == "COCONUTS" \
+                else orders.append(Order(product, pc_best_bid, -pc_best_bid_volume))
+        elif pc_best_ask / c_best_bid < 1.874:
+            orders.append(Order(product, c_best_bid, -c_best_bid_volume)) if product == "COCONUTS" \
+                else orders.append(Order(product, pc_best_ask, -pc_best_ask_volume))
 
-        order_depth: OrderDepth = state.order_depths[product]
-
-        # You can adjust the threshold according to the importance of dolphin sightings
-        sighting_threshold = 10
-
-        adjusted_fair_value = fair_value * (1 + dolphin_sightings / sighting_threshold)
-
-        # Buy the product if the adjusted fair value is higher than the best ask
-        if len(order_depth.sell_orders) > 0:
-            best_ask = min(order_depth.sell_orders.keys())
-            best_ask_volume = order_depth.sell_orders[best_ask]
-
-            if best_ask < adjusted_fair_value:
-                print("BUY", str(-best_ask_volume) + "x", best_ask)
-                return Order(product, best_ask, -best_ask_volume)
-
-        # Sell the product if the adjusted fair value is lower than the best bid
-        if len(order_depth.buy_orders) > 0:
-            best_bid = max(order_depth.buy_orders.keys())
-            best_bid_volume = order_depth.buy_orders[best_bid]
-
-            if best_bid > adjusted_fair_value:
-                print("SELL", str(best_bid_volume) + "x", best_bid)
-                return Order(product, best_bid, -best_bid_volume)
-            
-
-
-    def run(self, state: TradingState) -> Dict[str, List[Order]]:
-        result = {}
-        # Iterate over all the keys (the available products) contained in the order depths
-        for product in state.listings.keys():
-            order_depth: OrderDepth = state.order_depths[product]
-
-            orders: list[Order] = []
-
-            if product in state.market_trades:
-                self.trade_prices[product].extend(state.market_trades[product])
-
-            fair_value = self.calculate_fair_value(product)
-
-            if fair_value is None:
-                continue
-
-            # Process orders for PEARLS and BANANAS
-            if product in {"PEARLS", "BANANAS", "MAYBERRIES"}:
-                if len(order_depth.sell_orders) > 0:
-
-                # Sort all the available sell orders by their price,
-                # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
-
-                # Check if the lowest ask (sell order) is lower than the above defined fair value
-                    if best_ask < fair_value:
-
-                    # In case the lowest ask is lower than our fair value,
-                    # This presents an opportunity for us to buy cheaply
-                    # The code below therefore sends a BUY order at the price level of the ask,
-                    # with the same quantity
-                    # We expect this order to trade with the sell order
-                        print("BUY", str(-best_ask_volume) + "x", best_ask)
-                        orders.append(
-                            Order(product, best_ask, -best_ask_volume))
-
-            # The below code block is similar to the one above,
-            # the difference is that it find the highest bid (buy order)
-            # If the price of the order is higher than the fair value
-            # This is an opportunity to sell at a premium
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > fair_value:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(
-                            Order(product, best_bid, -best_bid_volume))
-
-            # Process orders for COCONUTS and PINA_COLADAS using statistical arbitrage
-            if product in {"COCONUTS", "PINA_COLADAS"}:
-                orders.append(
-                    self.process_statistical_arbitrage_orders(state, product, fair_value))
-                
-            if product == "DIVING_GEAR":
-                # Get the number of dolphin sightings
-                dolphin_sightings = state.observations['DOLPHIN_SIGHTINGS']
-                orders.append(
-                    self.process_diving_gear_orders(state, product, fair_value, dolphin_sightings))
-            
-            # Add all the above the orders to the result dict
-            result[product] = orders
-
-        return result
+        return orders
